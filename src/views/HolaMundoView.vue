604333eb6550a3704dc430bdcdc32440fbaded7d
klavijor <klavijorangel@gmail.com>
2025-03-26 17:16:13 -0500

    eliminación de todo mientras se estructura mejor

---
+++ src/views/HolaMundoView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="contenedor">
+      <h1>Hola mundo 👋</h1>
+      <p>Tu proyecto está listo para comenzar desde cero.</p>
+    </div>
+  </template>
+  
+  <script setup>
+  </script>
+  
+  <style scoped>
+  .contenedor {
+    padding: 2rem;
+    font-family: sans-serif;
+    text-align: center;
+  }
+  </style>
+  
